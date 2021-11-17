5ba576f279bf2e476a856a5fa5ba3ff991af8e5f
Robert Romero Medina <enanow41romero@gmail.com>
2021-11-16 22:44:11 -0400

    new changes

--- uploaderBackend/app.py
+++ uploaderBackend/app.py
@@ -35,6 +35,7 @@ origins = [
     "http://localhost",
     "http://localhost:8080",
     "http://localhost:3000",
+    "http://localhost:3000",
     # "https://todoapprobertfrontend.netlify.app"
 ]
 
